9940c7699ee80836c154b0a2897d5ad76741a266
João Marcos Rodrigues Pimentel <joaomarcosrpimentel@gmail.com>
2024-05-24 12:48:22 -0300

    feature(GESTAO-29): #comment Create CRUD functions for classes  #closed

--- src/cli/backend/csharp/minimal-api/webservice/crud-generator.ts
+++ src/cli/backend/csharp/minimal-api/webservice/crud-generator.ts
@@ -1,6 +1,6 @@
 import path from "path";
 import fs from "fs";
-import { Model, Module, isLocalEntity, isModule,  } from "../../../../../language/generated/ast.js";
+import { LocalEntity, Model, Module, isLocalEntity, isModule,  } from "../../../../../language/generated/ast.js";
 import { expandToStringWithNL } from "langium/generate";
 
 export function generateCrud(model: Model, target_folder: string) : void{
@@ -60,12 +60,22 @@ function generateMapGroups(modules : Module[]) : string {
         const mod_classes = mod.elements.filter(isLocalEntity);
         for (const classe of mod_classes) {
             mapGroups += `var ${classe.name.toLowerCase()} = ${mod.name.toLowerCase()}.MapGroup("/${classe.name}"); \n`
-            mapGroups += `${classe.name.toLowerCase()}.MapGet("/", async (${classe.name}Db db) =>\n await db.${classe.name}.ToListAsync());\n`
-            mapGroups += `${classe.name.toLowerCase()}.MapGet("/{id}", async (int id, ${classe.name}Db db) =>\n await db.${classe.name}.FindAsync(id); \n is ${classe.name} ${classe.name.toLowerCase()} \n ? Results.Ok(${classe.name.toLowerCase()}) \n : Results.NotFound()); \n \n`
-            mapGroups += `${classe.name.toLowerCase()}.MapPost("/", async (${classe.name} ${classe.name.toLowerCase()}, ${classe.name}Db db) => \n db.${classe.name}.Add(${classe.name}); \n await db.SavechangesAsync(); \n return Results.Created($"/${classe.name.toLowerCase()}/{${classe.name.toLowerCase()}.Id}", ${classe.name.toLowerCase()});\n }); \n \n`
-            mapGroups += `${classe.name.toLowerCase()}.MapPut("/{id}", async (int id, ${classe.name} input${classe.name}, ${classe.name}Db db) => \n var ${classe.name.toLowerCase()} = await db.${classe.name}.FindAsync(id); \n if (classe.name.toLowerCase() is null) return Results.NotFound(); \n // INPUTS AQUI) \n \n`
+            mapGroups += `${classe.name.toLowerCase()}.MapGet("/", async (${classe.name}Db db) =>\n    await db.${classe.name}.ToListAsync());\n`
+            mapGroups += `${classe.name.toLowerCase()}.MapGet("/{id}", async (int id, ${classe.name}Db db) =>\n    await db.${classe.name}.FindAsync(id); \n        is ${classe.name} ${classe.name.toLowerCase()} \n        ? Results.Ok(${classe.name.toLowerCase()}) \n        : Results.NotFound()); \n \n`
+            mapGroups += `${classe.name.toLowerCase()}.MapPost("/", async (${classe.name} ${classe.name.toLowerCase()}, ${classe.name}Db db) => \n{ \n    db.${classe.name}.Add(${classe.name}); \n    await db.SavechangesAsync(); \n    return Results.Created($"/${classe.name.toLowerCase()}/{${classe.name.toLowerCase()}.Id}", ${classe.name.toLowerCase()});\n}); \n \n`
+            mapGroups += `${classe.name.toLowerCase()}.MapPut("/{id}", async (int id, ${classe.name} input${classe.name}, ${classe.name}Db db) => \n{ \n    var ${classe.name.toLowerCase()} = await db.${classe.name}.FindAsync(id); \n    if (classe.name.toLowerCase() is null) return Results.NotFound(); ${generateInputs(classe)}\n    await db.SaveChangesAsync(); \n    return Results.NoContent() \n}); \n \n`
+            mapGroups += `${classe.name.toLowerCase()}.MapDelete("/{id}", async (int id, ${classe.name}Db db) => \n{ \n    if (await db.${classe.name}.FindAsync(id) is ${classe.name} ${classe.name.toLowerCase()}) { \n        db.${classe.name}.remover(${classe.name.toLowerCase()}); \n        await db.SaveChangesAsync(); \n        return Results.NoContent(); \n    } \n \n return Results.NotFound(); \n}); \n \n`
 
         }
     }
     return mapGroups;
+}
+
+function generateInputs(classe : LocalEntity) : string {    
+
+    let inputs = "";
+    for (const att of classe.attributes) {
+        inputs += `\n        ${classe.name.toLowerCase()}.${att.name} = input${classe.name}.${att.name}`
+    }
+    return inputs;
 }
