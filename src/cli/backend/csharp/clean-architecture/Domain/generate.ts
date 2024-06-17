87bacaf1625daf2a4dc261ca8ff9798ce19bb17d
João Marcos Rodrigues Pimentel <joaomarcosrpimentel@gmail.com>
2024-06-17 13:55:16 -0300

    feature(GESTAO-67): #comment Added Domain.Entities  #closed

--- src/cli/backend/csharp/clean-architecture/Domain/generate.ts
+++ src/cli/backend/csharp/clean-architecture/Domain/generate.ts
@@ -1,12 +1,18 @@
 import { Model } from "../../../../../language/generated/ast.js"
 import fs from "fs"
 import { generate as CommonGenerator } from "./Common/generate.js"
+import { generate as EntitiesGenerator } from "./Entities/generate.js"
+import { generate as ProjectGenerator} from "./project-generator.js"
 
 export function generate(model: Model, target_folder: string) : void {
     
     const common_folder = target_folder + "/Common"
+    const entities_folder = target_folder + "/Entities"
 
     fs.mkdirSync(common_folder, {recursive: true})
+    fs.mkdirSync(entities_folder, {recursive: true})
 
+    ProjectGenerator(model, target_folder)
     CommonGenerator(model, common_folder)
+    EntitiesGenerator(model, entities_folder)
 }
