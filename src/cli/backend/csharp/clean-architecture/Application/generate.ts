176e2fdd2a59e840041b0267a4b92a1cab382d0a
João Marcos <joaomrpimentel@gmail.com>
2024-06-19 14:50:45 -0300

    feature(GESTAO-89): #comment Added Application.DTOs  #closed

--- src/cli/backend/csharp/clean-architecture/Application/generate.ts
+++ src/cli/backend/csharp/clean-architecture/Application/generate.ts
@@ -3,17 +3,21 @@ import fs from "fs"
 import { generate as projectGenerator} from "./project-generator.js"
 import { generate as sharedGenerator} from "./Shared/generate.js"
 import { generate as servicesGenerator} from "./Services/generate.js"
+import { generate as DTOGenerator} from "./DTOs/generate.js"
 
 export function generate(model: Model, target_folder: string) : void {
     
     const Shared_folder = target_folder + "/Shared"
     const Services_Folder = target_folder + "/Services"
+    const DTOs_Folder = target_folder + "/DTOs"
 
     fs.mkdirSync(Shared_folder, {recursive: true})
     fs.mkdirSync(Services_Folder, {recursive: true})
+    fs.mkdirSync(DTOs_Folder, {recursive: true})
 
     projectGenerator(model, target_folder)
     sharedGenerator(model, Shared_folder)   
     servicesGenerator(model, Services_Folder)
+    DTOGenerator(model, DTOs_Folder)
 
 }
