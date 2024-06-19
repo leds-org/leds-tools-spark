6a167c012e5d96d148bfb66d573eac6efb100947
João Marcos <joaomrpimentel@gmail.com>
2024-06-19 13:02:11 -0300

    feature(GESTAO-93): #comment Added Application.Shared  #closed

--- src/cli/backend/csharp/clean-architecture/Application/generate.ts
+++ src/cli/backend/csharp/clean-architecture/Application/generate.ts
@@ -1,8 +1,15 @@
 import { Model } from "../../../../../language/generated/ast.js"
 import { generate as projectGenerator} from "./project-generator.js"
+import fs from "fs"
+import { generate as sharedGenerator} from "./Shared/generate.js"
 
 export function generate(model: Model, target_folder: string) : void {
     
+    const Shared_folder = target_folder + "/Shared"
+
+    fs.mkdirSync(Shared_folder, {recursive: true})
+
     projectGenerator(model, target_folder)
+    sharedGenerator(model, Shared_folder)   
 
 }
