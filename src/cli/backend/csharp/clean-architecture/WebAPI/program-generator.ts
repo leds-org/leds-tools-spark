65f28d1fe946472068e54c39ffc35b58fbcd7128
João Marcos Rodrigues Pimentel <joaomarcosrpimentel@gmail.com>
2024-06-21 15:37:42 -0300

    fix(GESTAO-107): #comment Changing program to fix dependency bugs  #closed

--- src/cli/backend/csharp/clean-architecture/WebAPI/program-generator.ts
+++ src/cli/backend/csharp/clean-architecture/WebAPI/program-generator.ts
@@ -1,5 +1,5 @@
 import { expandToString } from "langium/generate";
-import { Model } from "../../../../../language/generated/ast.js"
+import { Model, isLocalEntity, isModule } from "../../../../../language/generated/ast.js"
 import fs from "fs";
 import path from "path";
 
@@ -10,34 +10,117 @@ export function generate(model: Model, target_folder: string) : void {
 }
 function generateProgram(model: Model) : string {
     return expandToString`
-using ${model.configuration?.name}.Application.Configuration;
+using Microsoft.AspNetCore.OData;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+using Microsoft.OpenApi.Models;
+using Serilog;
+using ${model.configuration?.name}.Application.DTOs.Response;
+using ${model.configuration?.name}.Application.Services;
+using ${model.configuration?.name}.Application.Shared.Exceptions.Filters;
 using ${model.configuration?.name}.Infrastructure;
 using ${model.configuration?.name}.Infrastructure.Context;
 using ${model.configuration?.name}.WebApi.Extensions;
-using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.AddServiceDefaults();
+#region Adio do Serilog
+Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .WriteTo.File("logs/myapp.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+builder.Host.UseSerilog(Log.Logger);
+#endregion
 
+#region Add services to the container.
 builder.Services.ConfigurePersistenceApp(builder.Configuration);
 builder.Services.ConfigureApplicationApp();
+builder.Services.AddMvc()
+                .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+#endregion
 
+#region Adio do service do CORS
 builder.Services.ConfigureCorsPolicy();
+#endregion
 
-builder.Services.AddControllers();
+#region Adio do ODATA
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<BadRequestExceptionFilter>();
+    options.Filters.Add<DatabaseExceptionFilter>();
+})
+    .AddOData(options => options
+        .SkipToken()
+        .AddRouteComponents("api", GetEdmModel())
+        .Select()
+        .Filter()
+        .OrderBy()
+        .SetMaxTop(4)
+        .Count()
+        .Expand());
 
+static IEdmModel GetEdmModel()
+{
+    ODataConventionModelBuilder builder = new();
+
+    ${generateEntitySet(model)}
+    return builder.GetEdmModel();
+}
+#endregion
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.ODataConfiguration();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+
+    options.CustomSchemaIds(type => type.ToString());
+});
+
+//descomente para utilizar a requisio baseada na Role do User
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Coordenador", policy => policy.RequireRole("Coordenador"));
+});
+
+#region Adio das configuraes definidas no Builder Extension
+builder.AddConfiguration();
+builder.AddJwtAuthentication();
 
 var app = builder.Build();
 
 CreateDatabase(app);
+#endregion
 
-app.MapDefaultEndpoints();
-
-
+#region Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -45,27 +128,43 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseCors();
-
 app.UseHttpsRedirection();
-
+// Adio de autnticao e autorizao
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
+//app.UseRouting();
+app.MapAccountEndpoints();
+//descomente para utilizar a requisio baseada na Role do User
+app.MapControllers().RequireAuthorization("Coordenador");
+//app.MapControllers().RequireAuthorization();
+//app.MapControllers();
 
 app.Run();
+#endregion
 
-
-void CreateDatabase(WebApplication app)
+#region Cria banco de dados
+static void CreateDatabase(WebApplication app)
 {
     var serviceScope = app.Services.CreateScope();
     var dataContext = serviceScope.ServiceProvider.GetService<AppDbContext>();
-    dataContext?.Database.EnsureCreated();
-    dataContext?.Database.Migrate();
-
+        dataContext?.Database.EnsureCreated();
     // Carga no banco de dados
-    var sqlFile = "./Scripts/inserts.sql";
-    var sql = File.ReadAllText(sqlFile);
-    dataContext?.Database.ExecuteSqlRaw(sql);
-}`
+    //var sqlFile = "./Scripts/inserts.sql";
+    //var sql = File.ReadAllText(sqlFile);
+    //dataContext?.Database.ExecuteSqlRaw(sql);
+}
+#endregion`
 }
 
+function generateEntitySet(model: Model): string {
+    let identitySet = ""
+    const modules =  model.abstractElements.filter(isModule);
+    
+    for(const mod of modules) {
+        for(const cls of mod.elements.filter(isLocalEntity)) {
+            identitySet += `builder.EntitySet<${cls.name}ResponseDTO>("${cls.name}"); \n`
+        }
+    }
+    return identitySet
+}
--- src/cli/backend/csharp/clean-architecture/WebAPI/project-generator.ts
+++ src/cli/backend/csharp/clean-architecture/WebAPI/project-generator.ts
@@ -17,14 +17,31 @@ function generateProjectsln(model: Model) : string {
     <TargetFramework>net8.0</TargetFramework>
     <Nullable>enable</Nullable>
     <ImplicitUsings>enable</ImplicitUsings>
+	 <!--por padrão é true, alterei para false para usar o CreateDatabase-->
+    <InvariantGlobalization>false</InvariantGlobalization>
     <UserSecretsId></UserSecretsId>
     <DockerDefaultTargetOS>Linux</DockerDefaultTargetOS>
     <DockerComposeProjectPath>..\\docker-compose.dcproj</DockerComposeProjectPath>
   </PropertyGroup>
 
   <ItemGroup>
-    <PackageReference Include="Microsoft.VisualStudio.Azure.Containers.Tools.Targets" Version="1.20.1" />
+    <PackageReference Include="AWSSDK.Core" Version="3.7.303.23" />
+    <PackageReference Include="AWSSDK.SecretsManager" Version="3.7.302.58" />
+    <PackageReference Include="Microsoft.AspNet.WebApi.Cors" Version="5.3.0" />
+    <PackageReference Include="Microsoft.AspNetCore.Authentication.JwtBearer" Version="8.0.3" />
+    <PackageReference Include="Microsoft.AspNetCore.OData" Version="8.2.5" />
+    <PackageReference Include="Microsoft.AspNetCore.OData.NewtonsoftJson" Version="8.2.0" />
+    <PackageReference Include="Microsoft.EntityFrameworkCore.Tools" Version="8.0.2">
+      <PrivateAssets>all</PrivateAssets>
+      <IncludeAssets>runtime; build; native; contentfiles; analyzers; buildtransitive</IncludeAssets>
+    </PackageReference>
+    <PackageReference Include="Microsoft.VisualStudio.Azure.Containers.Tools.Targets" Version="1.19.5" />
+    <PackageReference Include="Newtonsoft.Json" Version="13.0.3" />
+    <PackageReference Include="Serilog" Version="3.1.1" />
+    <PackageReference Include="Serilog.AspNetCore" Version="8.0.1" />
+    <PackageReference Include="Serilog.Extensions.Hosting" Version="8.0.0" />
     <PackageReference Include="Swashbuckle.AspNetCore" Version="6.4.0" />
+    <PackageReference Include="Swashbuckle.AspNetCore.SwaggerUI" Version="6.5.0" />
   </ItemGroup>
 
   <ItemGroup>
