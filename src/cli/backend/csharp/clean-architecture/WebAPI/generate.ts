5958d94a6f5916f14d377e4128e8e5c82122808e
João Marcos Rodrigues Pimentel <joaomarcosrpimentel@gmail.com>
2024-06-24 12:30:24 -0300

    feature(GESTAO-103): #comment Added WebAPI.Properties  #closed

--- src/cli/backend/csharp/clean-architecture/WebAPI/generate.ts
+++ src/cli/backend/csharp/clean-architecture/WebAPI/generate.ts
@@ -4,20 +4,24 @@ import { generate as helperGenerator } from "./helpers-generator.js"
 import { generate as programGenerator } from "./program-generator.js"
 import { generate as configGenerator } from "./.config/generate.js"
 import { generate as extensionsGenerator } from "./Extensions/generate.js"
+import { generate as propertiesGenerator } from "./Properties/generate.js"
 import fs from "fs"
 
 export function generate(model: Model, target_folder: string) : void {
 
     const config_folder = target_folder + "/.config"
     const extensions_folder = target_folder + "/Extensions"
+    const properties_folder = target_folder + "/Properties"
 
     fs.mkdirSync(config_folder, {recursive: true})
     fs.mkdirSync(extensions_folder, {recursive: true})
+    fs.mkdirSync(config_folder, { recursive: true })
 
     projectGenerator(model, target_folder)
     helperGenerator(model, target_folder)
     programGenerator(model, target_folder)
     configGenerator(model, config_folder)
     extensionsGenerator(model, extensions_folder)
+    propertiesGenerator(model, properties_folder)
 
 }
